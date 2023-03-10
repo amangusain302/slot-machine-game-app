0e8e5429a80500fbee8c2368677dd2203dfed41e
amangusain302 <amangusain302@gmail.com>
2023-03-10 17:14:34 +0530

    create new app slot machine app

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+import Heading from "./component/heading";
+export default function App() {
+  return <Heading />;
+}
---
+++ src/component/heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Heading() {
+  return(
+    <div className='heading_style'>
+        <div className="heading_text">
+            🎰 Welcome To <b>Slot Machine Game</b> 🎰
+        </div>
+    </div>
+  )
+}
