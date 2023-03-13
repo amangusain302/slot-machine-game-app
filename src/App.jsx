361824cd7285cb3d802551cee0c46278c565b8e6
amangusain302 <amangusain302@gmail.com>
2023-03-13 21:12:28 +0530

    working jackpot machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { flushSync } from "react-dom";
 import "./App.css";
 import Heading from "./component/heading";
 import Slots from "./component/Slots";
 export default function App() {
-  const emojies = [
-    {
-      x: "🤣",
-      y: "🤣",
-      z: "😍",
-    },
-    {
-        x: "🚗",
-        y: "🤣",
-        z: "😍",
-      },
-      {
-        x: "🎰",
-        y: "🎰",
-        z: "🎰",
-      }
-  ];
+  const [emojies, setemojies] = useState([]);
+  const emojiArray = ["🎰", "😍", "🚗", "💸"];
+  const [x, setX] = useState(null);
+  const [y, setY] = useState(null);
+  const [z, setZ] = useState(null);
+  const [know, setknow] = useState(false);
+  // console.log(emojies, "fist");
+  const shuffleHandler = async () => {
+    for (var i = 0; i <= 9000; i++) {
+    // setTimeout( () => {
+        var randomIndex =  Math.floor(Math.random() * emojiArray.length);
+        flushSync(() => setX(emojiArray[randomIndex]));
+        randomIndex =  Math.floor(Math.random() * emojiArray.length);
+        flushSync(() => setY(emojiArray[randomIndex]));
+        randomIndex = Math.floor(Math.random() * emojiArray.length);
+        flushSync(() => setZ(emojiArray[randomIndex]));
+      // }, 100);
+      // if (i === 9000) setknow(true);
+    }
+    console.log(know)
+    // clearInterval(intervalLoop);
+    // if (know ===true){
+    setemojies([...emojies, { x, y, z }]);
+    setknow(false)
+  // }
+    console.log(emojies, "secd");
+  };
   return (
     <>
       <Heading />
@@ -28,10 +39,20 @@ export default function App() {
       </div> */}
       <div className="container">
         <div className="slot_box">
-          <Slots x={emojies[0].x} y={emojies[0].y} z={emojies[0].z} />
-          <Slots x={emojies[1].x} y={emojies[1].y} z={emojies[1].z} />
-          <Slots x={emojies[2].x} y={emojies[2].y} z={emojies[2].z} />
+          {emojies.map((Element) => {
+            return <Slots x={Element.x} y={Element.y} z={Element.z} />;
+          })}
+          {/* <Slots x={emojies[1].x} y={emojies[1].y} z={emojies[1].z} />
+          <Slots x={emojies[2].x} y={emojies[2].y} z={emojies[2].z} /> */}
         </div>
+        <div className="container_shuffler">
+          <div className="container_shuffler_one">{x}</div>
+          <div className="container_shuffler_two">{y}</div>
+          <div className="container_shuffler_three">{z}</div>
+        </div>
+        <button className="contianer_jackpot_btn" onClick={shuffleHandler}>
+          JACKPOT
+        </button>
       </div>
     </>
   );
--- src/component/Slots.jsx
+++ src/component/Slots.jsx
@@ -4,26 +4,19 @@ export default function Slots(props) {
     const { x, y, z} = props;
     if( (x===y) && (y===z))
     {
+      var message = "This Is Matching!" ;
+    }
+    else
+    {
+      var message = "This Is Not Matching!" ;
+    }
   return (
     <>
      <div className='slot_box_inner'>
      <h3>{`${x} ${y} ${z}`}</h3>
-    <h3>This Is Matching</h3>
+    <h3>{message}</h3>
         </div>
         <hr/>
     </>
   )
-    }
-    else
-    {
-        return(
-            <>
-           <div className='slot_box_inner' >
-     <h3>{`${x} ${y} ${z}`}</h3>
-    <h3>This Is Not Matching</h3>
-        </div>
-        <hr/>
-           </>
-        )
-    }
 }
